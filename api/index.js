4b4bd49c5fa128fbf3104c10792219b22de01da1
johnn <johnfanidis@gmail.com>
2026-01-13 18:24:59 +0200

    Fixed build configuration for Prisma

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
-import { serve } from "@react-router/serve";
+import { createRequestHandler } from "@react-router/node";
 import * as build from "../build/server/index.js";
 
-export default serve(build);
+const handler = createRequestHandler(build, "production");
+
+export default handler;
